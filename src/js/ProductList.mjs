9c29f73a143421044cbe7155bd9523ed06a53b1c
Boyd Querubin <boydisaacq@gmail.com>
2025-05-24 07:40:07 -0600

    productlist

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -27,6 +27,9 @@ export default class ProductList {
   }
 
   renderList(list) {
+    // const htmlStrings = list.map(productCardTemplate);
+    // this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+    
     renderListWithTemplate(productCardTemplate, this.listElement, list);
 
   }
