2b1c6fe3bf72ee5f3374709dbee681f1d0038450
Boyd Querubin <boydisaacq@gmail.com>
2025-05-24 07:59:58 -0600

    product update

--- src/js/product.js
+++ src/js/product.js
@@ -5,19 +5,25 @@ import ProductDetails from './ProductDetails.mjs';
 
 loadHeaderFooter();
 
-// Get product ID from URL
-const productId = getParam('product');
-if (!productId) {
-  console.error('No product ID provided in URL');
-} else {
-  const dataSource = new ProductData('tents');
-  const product = new ProductDetails(productId, dataSource);
+const dataSource = new ProductData("tents");
+const productID = getParam("product");
 
-  // Initialize product details
-  product.init();
+const product = new ProductDetails(productID, dataSource);
+product.init();
 
-  (async () => {
-    const productData = await dataSource.findProductById(productId);
-    console.log('Product data:', productData);
-  })();
-}
+// // Get product ID from URL
+// const productId = getParam('product');
+// if (!productId) {
+//   console.error('No product ID provided in URL');
+// } else {
+//   const dataSource = new ProductData('tents');
+//   const product = new ProductDetails(productId, dataSource);
+
+//   // Initialize product details
+//   product.init();
+
+//   (async () => {
+//     const productData = await dataSource.findProductById(productId);
+//     console.log('Product data:', productData);
+//   })();
+// }
